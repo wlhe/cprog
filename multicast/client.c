c2fc2d1020590e03a0796c671b03437b63928689
wlhe <wlhe92@gmail.com>
2017-10-07 21:37:37 +0800

    multicast to all interface

--- multicast/client.c
+++ multicast/client.c
@@ -13,6 +13,7 @@
 #define MSG "multicast message"
 int main()
 {
+
     int sock;
     struct sockaddr_in mult_addr;
     sock = socket(AF_INET, SOCK_DGRAM, 0);
@@ -26,15 +27,43 @@ int main()
     mult_addr.sin_addr.s_addr = inet_addr(MULT_ADDR);
 
     socklen_t l = sizeof(mult_addr);
+
     unsigned char ttl = 20;
     l = sizeof(ttl);
     setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, (void *)&ttl, l);
     l = sizeof(mult_addr);
 
-    while (1) {
-        sendto(sock, MSG, sizeof(MSG), 0, (struct sockaddr *)&mult_addr, l);
-        printf("SEND to : %s buf: %s\n", inet_ntoa(mult_addr.sin_addr), MSG);
+    struct ifreq ifr[10];
+    struct ifconf ifc;
+    ifc.ifc_req = ifr;
+    ifc.ifc_len = sizeof(ifr);
 
+    if (ioctl(sock, SIOCGIFCONF, &ifc) < 0) {
+        printf("ioctl SIOCGIFCONF error\n");
+        return -1;
+    }
+    struct sockaddr_in *addr = NULL;
+    int n = (ifc.ifc_len / sizeof(struct ifreq));
+    int i = 0;
+
+    // forbid loop, not send to ownself
+    // common this for local machine test
+    unsigned char loop = 0;
+    setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP, &loop, sizeof(loop));
+
+    while (1) {
+        for (i = 0; i < n; i++) {
+            addr = (struct sockaddr_in *)&(ifr[i].ifr_addr);
+            // mask some interface noused, eg. localhost
+            if (strcmp("127.0.0.1", inet_ntoa(addr->sin_addr)) == 0) {
+                continue;
+            }
+            l = sizeof(struct sockaddr_in);
+            setsockopt(sock, IPPROTO_IP, IP_MULTICAST_IF, (void *)addr, l);
+            sendto(sock, MSG, sizeof(MSG), 0, (struct sockaddr *)&mult_addr, l);
+            printf("SEND to : %s buf: %s\n", inet_ntoa(addr->sin_addr), MSG);
+        }
+        printf("---------------------\n");
         if (getchar() == 'q') {
             break;
         }
--- multicast/server.c
+++ multicast/server.c
@@ -10,6 +10,7 @@
 
 #define MULT_ADDR "224.0.0.250"
 #define MULT_PORT 9999
+
 int main()
 {
     struct sockaddr_in addr;
@@ -34,23 +35,51 @@ int main()
 
     memset(&mreq, 0, sizeof(mreq));
     mreq.imr_multiaddr.s_addr = inet_addr(MULT_ADDR);
-    mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+    struct ifreq ifr[10];
+    struct ifconf ifc;
+    ifc.ifc_req = ifr;
+    ifc.ifc_len = sizeof(ifr);
+
+    if (ioctl(sock, SIOCGIFCONF, &ifc) < 0) {
+        printf("ioctl SIOCGIFCONF error\n");
+        return -1;
+    }
+    struct sockaddr_in *paddr = NULL;
+    int m = (ifc.ifc_len / sizeof(struct ifreq));
+    int i = 0;
 
     char buf[64];
     socklen_t l = sizeof(addr);
-    if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
-        printf("failed to join multicast group\n");
-        close(sock);
-        return -1;
+
+    for (i = 0; i < m; i++) {
+        // need add all interface to multicast group
+        paddr = (struct sockaddr_in *)&(ifr[i].ifr_addr);
+        if (strcmp("127.0.0.1", inet_ntoa(paddr->sin_addr)) == 0) {
+            continue;
+        }
+
+        mreq.imr_interface.s_addr = paddr->sin_addr.s_addr;
+        if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+            printf("failed to join multicast group\n");
+            close(sock);
+            return -1;
+        }
     }
     while (1) {
         int n = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &l);
         if (n > 0) {
             printf("RECIVE from %s => buf: %s\n", inet_ntoa(addr.sin_addr), buf);
         }
+        printf("---------------------\n");
+    }
+    for (i = 0; i < m; i++) {
+        if (strcmp("127.0.0.1", inet_ntoa(paddr->sin_addr)) == 0) {
+            continue;
+        }
+        setsockopt(sock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq));
     }
 
-    setsockopt(sock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &mreq, sizeof(mreq));
     close(sock);
     return 0;
 }
